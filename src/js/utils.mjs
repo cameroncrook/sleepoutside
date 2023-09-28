efbcd1dfc9534d8d50017e0a6a1eb953c959d409
cameroncrook <cameronbcrook@gmail.com>
2023-09-28 09:06:55 -0600

    fixed partials error

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -70,8 +70,8 @@ export async function loadTemplate(path) {
 }
 
 export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate("../public/partials/header.html");
-  const footerTemplate = await loadTemplate("../public/partials/footer.html");
+  const headerTemplate = await loadTemplate("/partials/header.html");
+  const footerTemplate = await loadTemplate("/partials/footer.html");
   
   const header = document.querySelector('#main-header');
   renderWithTemplate(headerTemplate, header);

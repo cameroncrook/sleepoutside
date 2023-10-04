661be7740d2a3ab7e29194110bcf68af33e65ff4
cameroncrook <cameronbcrook@gmail.com>
2023-10-04 17:45:14 -0600

    Added checkoutfeature

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,17 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import CheckoutProcess from "./CheckoutProccess.mjs";
 
 loadHeaderFooter();
+
+const checkout = new CheckoutProcess("so-cart", ".orderSummary");
+checkout.displaySummary();
+
+document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    checkout.checkout();
+});
+
+
+
+
--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -31,24 +31,60 @@ function cartItemTemplate(item) {
       document.querySelector(this.parentSelector).innerHTML = htmlItems.join("");
       
       if (cartItems.length > 0) {
-        let cartTotal = 0;
+        
+        const total = this.getCartTotal(cartItems);
+        let totalElement = document.getElementById('cart-total');
+        totalElement.textContent += total;
+        totalElement.className = "";
 
+        document.getElementById('checkout-btn').classList.remove('d-none');
+      } 
+    }
+    checkout() {
+      const total = this.getCartTotal();
+      const itemsCount = this.getCartCount();
+      const tax = .06;
+      const taxPrice = total * tax;
+
+      let shippingPrice = 0;
+      if (itemsCount > 0) {
+        shippingPrice = 10 + (2 * (itemsCount - 1));
+      }
+
+      const finalPrice = taxPrice + shippingPrice;
+    }
+    getCartTotal() {
+      const cartItems = getLocalStorage(this.key);
+      let cartTotal = 0;
+
+      if (cartItems.length > 0) {
         cartItems.forEach(item => {
           cartTotal += item.FinalPrice;
         });
-
+  
         let formatter = new Intl.NumberFormat('en-US', {
           style: 'currency',
           currency: 'USD', 
           minimumFractionDigits: 2, 
           maximumFractionDigits: 2 
         });
-
+  
         const total = formatter.format(cartTotal);
-        
-        let totalElement = document.getElementById('cart-total');
-        totalElement.textContent += total;
-        totalElement.className = "";
-      } 
+  
+        return total
+      } else {
+        return
+      }
+      
+    }
+    getCartCount() {
+      const cartItems = getLocalStorage(this.key);
+      if (cartItems) {
+        const cartCount = cartItems.length;
+
+        return cartCount
+      } else {
+        return
+      }
     }
   }
